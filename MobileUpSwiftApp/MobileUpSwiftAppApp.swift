2e419c2b207da7228c2839bec48ea34e5b870dab
Og_Vobla <gggg26705@gmail.com>
2024-08-13 18:48:40 +0300

    Initial Commit

---
+++ MobileUpSwiftApp/MobileUpSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileUpSwiftAppApp.swift
+//  MobileUpSwiftApp
+//
+//  Created by Радэль Зубаиров on 13.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MobileUpSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
